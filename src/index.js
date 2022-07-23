0c9b77c01f3726a74a24e788062754acdbb0518d
Donavyn <donavynpence@yahoo.com>
2022-07-23 14:00:46 -0700

    add dev dependencies, cleared clutter

--- src/index.js
+++ src/index.js
@@ -2,17 +2,3 @@ import * as ship from './modules/ship'; /* < that makes anything in that module 
 import './style.css';
 
 console.log('ef');
-
-const name = 'carl';
-
-const person = { first: name };
-
-console.log(person);
-
-const sayHelloLinting = (
-  fNam,
-  asdajhdakshak,
-  ajsbdkjabdkjadbkjasdsbksabdskjasbkja,
-) => {
-  console.log(`Hello linting, ${fName}`);
-};
